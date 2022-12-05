f29ed4ee60085a5e1b9a4abe387d14fd70deaa21
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2022-12-05 22:35:46 +0530

    Create sum of first n pos  int.cpp

---
+++ Expressions/sum of first n pos  int.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a positive integer : ";
+	cin>>n;
+	cout<<"sum of first n positive integers is:"<<n*(n+1)/2<<"\n";
+	return 0;
+}
