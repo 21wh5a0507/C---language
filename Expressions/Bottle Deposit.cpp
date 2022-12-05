6c1177e124158c3669df4fc44562cab7f072edd2
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2022-12-05 23:13:48 +0530

    Create Bottle Deposit.cpp

---
+++ Expressions/Bottle Deposit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	float x,y,z;
+	cout<<"Enter 1st Container: ";
+	cin>>x;
+	cout<<"Enter 2nd Container: ";
+	cin>>y;
+	z=x*0.10 + y*0.25;
+	printf("$%.2f",z);
+	return 0;
+}
