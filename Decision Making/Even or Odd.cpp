a2b47518776793e4e9b24879ecafc6a16b45bda2
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2022-12-07 12:19:45 +0530

    Create Even or Odd.cpp

---
+++ Decision Making/Even or Odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter an integer: ";
+	cin>>n;
+	if(n%2==0){
+		cout<<"given integer is even";
+	}
+	else{
+		cout<<"given integer is odd";
+	}
+	return 0;
+}
