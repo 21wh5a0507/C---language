aaaaa1505ddb32de25ad01a82ff200ba07ef4dca
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2022-12-11 23:12:27 +0530

    Create Sum of the digits using loops.cpp

---
+++ Repetitions/Simple loops/Sum of the digits using loops.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int number,sum=0,m;
+	cout<<"enter a number :";
+	cin>>number;
+	while(n>0){
+		m=n%10;
+		sum=sum+m;
+		n=n/10;
+	}
+	cout<<"Sum of digits : "<<sum;
+	return 0;
+}
