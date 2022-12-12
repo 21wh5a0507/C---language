f9fa93cb3b5a94334325315094b6bff34b63b2d2
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2022-12-12 16:29:55 +0530

    Update Sum of Odd integers.cpp

--- Repetitions/Simple loops/Sum of Odd integers.cpp
+++ Repetitions/Simple loops/Sum of Odd integers.cpp
@@ -1,18 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int n,sum=0,count=0;
+	int n,sum=0,a=1;
 	cout<<"enter a number :";
 	cin>>n;
-    for(int i=1;i<=100;i++)
+    for(int i=1;i<=n;i++)
     {
-	   if(i%2!=0)
-	   {
-	      count++;
-	      sum=sum+i;
-	    if(count==n)
-	   	cout<<sum;
-	   }
+    	sum=sum+a;
+		a=a+2; 
     }
+    cout<<"Sum of Odd Integer : "<<sum;
 	return 0;
 }
