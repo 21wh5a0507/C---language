93caae78dbd11aa8d90692cd9a11c3f8a29b63b7
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2023-01-03 00:18:55 +0530

    Create Approximate PI.cpp

---
+++ Functions/Functions2/Approximate PI.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+double approximatePI(int n)
+{
+double sign=1,s=0,d=2;
+for(int i=1;i<=n;i++)
+{
+s+=1/(d*(d+1)*(d+2))*sign;
+d+=2;
+sign*=-1;
+}
+return s*4+3;
+}
+int main()
+{
+int num;
+cout<<"Enter Number:";
+cin>>num;
+cout<<"The Approximate is "<<approximatePI(num);
+return 0;
+}
+
