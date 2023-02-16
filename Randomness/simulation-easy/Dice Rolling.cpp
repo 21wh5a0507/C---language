528d60a20c86f0edfdab88ea253fcc3dc4990e74
21wh5a0507 <21wh5a0507@bvrithyderabad.edu.in>
2023-02-16 16:00:25 +0530

    Update Dice Rolling.cpp

--- Randomness/simulation-easy/Dice Rolling.cpp
+++ Randomness/simulation-easy/Dice Rolling.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 string rollDice(){
-	int a,b,c,d,e;
+	int a,b,c;
 	srand(time(0)); 
 		a=rand()%6+1;
 		b=rand()%6+1;
 		c=rand()%6+1;
-		d=rand()%6+1;
-		e=rand()%6+1;
-	cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<"\n";
-	if(a==b && a==c && a==d && a==e)
+	cout<<a<<" "<<b<<" "<<c<<"\n";
+	if(a==b && a==c)
 	return "Yahoo";
 	else 
 	return "Try Again";
